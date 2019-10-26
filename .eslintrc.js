3a9ca98e86f1bb765feaf1a45df5602b428db870
maxim <tyminko@gmail.com>
2019-10-26 21:50:16 +0200

    refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "standard"
-};
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
